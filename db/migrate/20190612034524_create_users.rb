17eb00c1f78c06feefd1c93362bece81c2c524eb
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-11 23:56:09 -0400

    Created a users table

---
+++ db/migrate/20190612034524_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.integer :location_id
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :address
+      t.string :zipcode
+    end
+  end
+end
