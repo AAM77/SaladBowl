51bb6588ea27f9cf4348ce6f6d8dd4fda1800089
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-12 22:55:51 -0400

    Added unnecessary test files. May remove them or use them.

---
+++ test/models/salad_bar_location_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SaladBarLocationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/salad_bowl_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SaladBowlTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
