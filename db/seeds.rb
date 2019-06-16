630675b9b85e859f69fe7fdcc78bafe7df069e94
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-15 23:14:09 -0400

    Added code to generate the SaladBowls.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,57 +1,67 @@
 require 'faker'
 
-# #############
-# # LOCATIONS #
-# #############
-# # create 10 locations
-# 10.times do
-#   Location.create(
-#     city: Faker::Address.unique.city,
-#     state: Faker::Address.unique.state,
-#     country: 'United States'
-#   )
-# end
-#
-# # An array of all the ids for current locations
-# loc_ids = Location.all.map { |l| l.id }
-#
-# ##############
-# # SALAD BARS #
-# ##############
-# 10.times do
-#   new_salad_bar = SaladBar.create(
-#     name: Faker::Vehicle.unique.manufacture
-#   )
-#   rand(3).times do
-#     new_salad_bar.locations.push(Location.find(loc_ids[rand(loc_ids.length)]))
-#   end
-# end
-#
-#
-# #########
-# # USERS #
-# #########
-# # create 10 users, assign a location, and persist/save it
-# 10.times do
-#   new_user = User.new(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     username: Faker::Ancient.unique.hero,
-#     email: "#{Faker::Creature::Animal.unique.name}@info.com",
-#     password_digest: BCrypt::Password.create(Faker::Games::Pokemon.name),
-#     address: Faker::Address.street_address,
-#     zipcode: Faker::Address.unique.zip_code
-#   )
-#
-#   new_user.location = Location.find(loc_ids[rand(loc_ids.length)])
-#   new_user.save
-# end
-#
 
-users = User.all.map { |u| u.id }
+
+#############
+# LOCATIONS #
+#############
+# create 10 locations
+10.times do
+  Location.create(
+    city: Faker::Address.unique.city,
+    state: Faker::Address.unique.state,
+    country: 'United States'
+  )
+end
+
+
+
+##############
+# SALAD BARS #
+##############
+
+# An array of all the ids for current locations
+loc_ids = Location.all.map { |l| l.id }
+
+10.times do
+  new_salad_bar = SaladBar.create(
+    name: Faker::Vehicle.unique.manufacture
+  )
+  rand(3).times do
+    new_salad_bar.locations.push(Location.find(loc_ids[rand(loc_ids.length)]))
+  end
+end
+
+
+
+#########
+# USERS #
+#########
+# create 10 users, assign a location, and persist/save it
+
+10.times do
+  new_user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Ancient.unique.hero,
+    email: "#{Faker::Creature::Animal.unique.name}@info.com",
+    password_digest: BCrypt::Password.create(Faker::Games::Pokemon.name),
+    address: Faker::Address.street_address,
+    zipcode: Faker::Address.unique.zip_code
+  )
+
+  new_user.location = Location.find(loc_ids[rand(loc_ids.length)])
+  new_user.save
+end
+
+
+
 ###############
 # PREFERENCES #
 ###############
+
+# create an array of existing user ids
+users = User.all.map { |u| u.id }
 
 allergies = [
   'milk',
@@ -89,31 +99,46 @@ true_false = [true, false]
   new_preference.save
 end
 
-# t.string :allergy
-# t.boolean :vegetarian
-# t.boolean :kosher
-# t.boolean :halal
-# t.boolean :fasting
-# t.boolean :bodybuilding
-# t.boolean :bulking
-# t.boolean :weight_loss
-
-# ##############
-# # CATEGORIES #
-# ##############
-# # create 10 categories
-# 10.times do
-#   Category.create(
-#     name: Faker::Food.unique.vegetables
-#   )
-# end
 
 
-###############
-# INGREDIENTS #
-###############
+##############
+# CATEGORIES #
+##############
+# create 10 categories
+10.times do
+  Category.create(
+    name: Faker::Food.unique.vegetables
+  )
+end
+
 
 
 ###############
 # SALAD BOWLS #
 ###############
+
+meal_time_frames = ['breakfast', 'lunch', 'dinner']
+true_false = [true, false]
+
+10.times do
+  SaladBowl.create(
+    name: Faker::Food.name,
+    meal_time_frame: meal_time_frames.sample,
+    favorite: true_false.sample
+  )
+end
+
+
+
+
+
+
+
+# t.string "name"
+# t.integer "calories"
+# t.integer "fat"
+# t.integer "cholesterol"
+# t.integer "sodium"
+# t.integer "carbohydrates"
+# t.integer "sugar"
+# t.integer "protein"
