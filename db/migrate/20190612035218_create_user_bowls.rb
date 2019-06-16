ae9b97f910263717d9b3c113490a4510ef5fc199
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-15 23:10:23 -0400

    It's bad practice, but I'm working alone, so it was simly easier to modify them directly to fixed the typos.

--- db/migrate/20190612035218_create_user_bowls.rb
+++ db/migrate/20190612035218_create_user_bowls.rb
@@ -2,7 +2,7 @@ class CreateUserBowls < ActiveRecord::Migration[5.2]
   def change
     create_table :user_bowls do |t|
       t.belongs_to :user, index: true
-      t.belongs_to :bowl, index: true
+      t.belongs_to :salad_bowl, index: true
     end
   end
 end
--- db/migrate/20190612035246_create_bowl_ingredients.rb
+++ db/migrate/20190612035246_create_bowl_ingredients.rb
@@ -1,7 +1,7 @@
 class CreateBowlIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :bowl_ingredients do |t|
-      t.belongs_to :bowl, index: true
+      t.belongs_to :salad_bowl, index: true
       t.belongs_to :ingredient, index: true
     end
   end
