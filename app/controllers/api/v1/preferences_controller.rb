8a1e71939ae6be86cfcc5335760371b7cc84b46a
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 23:26:07 -0400

    Added private methods set_preference & preference_params. Removed unnecesary actions (new and edit). Added relevant code to actions to render the data as JSON.

--- app/controllers/api/v1/preferences_controller.rb
+++ app/controllers/api/v1/preferences_controller.rb
@@ -1,22 +1,53 @@
 class Api::V1::PreferencesController < ApplicationController
+  before_action :set_preference, only: [:show, :update, :destroy]
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @preferences = Prefence.all
+    render json: @preferences
   end
 
   def show
+    render json: @preference
   end
 
-  def edit
+  def create
+    @preference = Preference.new(preference_params)
+
+    if @preference.save
+      render json: @preference, status: :created, location: @preference
+    else
+      render json: @preference.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @preference.save
+      render json: @preference, status: :accepted
+    else
+      render json: @preference.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @preference.destroy
   end
 end
+
+private
+
+  def set_preference
+    @preference = Preference.find(params[:id])
+  end
+
+  def preference_params
+    params.require(:preference).permit(
+      :user_id,
+      :allergy,
+      :vegetarian,
+      :kosher,
+      :halal,
+      :fasting,
+      :bodybuilding,
+      :bulkin,
+      :weight_loss
+    )
+  end
