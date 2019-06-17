8d86c5bf6776fb87e705f1773176cd17c92b24b7
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 23:14:56 -0400

    Added private methods set_location & location_params. Removed unnecesary actions (new and edit). Added relevant code to actions to render the data as JSON.

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,22 +1,48 @@
 class Api::V1::LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :update, :destroy]
+
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @locations = Location.all
+    render json: @locations
   end
 
   def show
+    render json: @location
   end
 
-  def edit
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      render json: @location, status: :created, location: @location
+    else
+      render json: @location.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @location.save
+      render json: @location, status: :updated, location: @location
+    else
+      render json: @location.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @location.destroy
   end
 end
+
+private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(
+      :city,
+      :state,
+      :country
+    )
+  end
