da25c19967f1e30f54a7253b5bdfb033ac50d9dc
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 23:10:58 -0400

    Added private methods set_salad_bowl & salad_bowl_params. Removed unnecesary actions (new and edit). Added relevant code to actions to render the data as JSON.

--- app/controllers/api/v1/salad_bowls_controller.rb
+++ app/controllers/api/v1/salad_bowls_controller.rb
@@ -1,33 +1,45 @@
 class Api::V1::SaladBowlsController < ApplicationController
+  before_action :set_salad_bowl, only: [:show, :update, :destroy]
+
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @salad_bowls = SaladBowl.all
+    render json: @salad_bowls
   end
 
   def show
-    bowl = SaladBowl.find(params[:id])
-    respond_to do |format|
-      format.json { render json: bowl }
+    render json: @salad_bowl
+  end
+
+  def create
+    @salad_bowl = SaladBowl.new(salad_bowl_params)
+
+    if @salad_bowl.save
+      render json: @salad_bowl, status: :created, location: @salad_bowl
+    else
+      render json: @salad_bowl.errors, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
-
   def update
+    if @salad_bowl.save
+      render json: @salad_bowl, status: :updated, location: @salad_bowl
+    else
+      render json: @salad_bowl.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @salad_bowl.destroy
   end
 
   private
 
-    def bowl_params
-      params.require(:bowl).permit(
+    def set_salad_bowl
+      @salad_bowl = SaladBowl.find(params[:id])
+    end
+
+    def salad_bowl_params
+      params.require(:salad_bowl).permit(
         :name,
         :meal_time_frame,
         :favorite
