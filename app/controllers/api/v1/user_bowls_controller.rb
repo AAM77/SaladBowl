c5f4d2a98c608799e78547810b975cc957ef52f4
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 23:05:32 -0400

    Added private methods set_user_bowl & user_bowl_params. Removed unnecesary actions. Added relevant code to actions to render the data as JSON.

--- app/controllers/api/v1/user_bowls_controller.rb
+++ app/controllers/api/v1/user_bowls_controller.rb
@@ -1,22 +1,47 @@
 class Api::V1::UserBowlsController < ApplicationController
+  before_action :set_user_bowl, only: [:show, :update, :destroy]
+
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @user_bowls = UserBowl.all
+    render json: @users
   end
 
   def show
+    render json: @user_bowl
   end
 
-  def edit
+  def create
+    @user_bowl = UserBowl.new(user_bowl_params)
+
+    if @user_bowl.save
+      render json: @user_bowl, status: :created, location: @user_bowl
+    else
+      render json: @user_bowl.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @user_bowl.save
+      render json: @user_bowl, status: :updated, location: @user_bowl
+    else
+      render json: @user_bowl.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @user_bowl.destroy
   end
 end
+
+private
+
+  def set_user_bowl
+    @user_bowl = UserBowl.find(params[:id])
+  end
+
+  def user_bowl_params
+    params.require(:user_bowl).permit(
+      :user_id,
+      :salad_bowl_id
+    )
+  end
