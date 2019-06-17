8cbeeb6b5208cc085e2c3199438a3b8efb0ba2cc
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 22:58:43 -0400

    Removed unnecessary actions. Added in a set_user & before action. Added in code to each action to render the data as JSON.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,57 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+  # GET /users
   def index
+    @users = User.all
+    render json: @users
   end
 
-  def new
-  end
-
-  def create
-  end
-
+  # GET /users/:id
   def show
+    render json: @user
   end
 
-  def edit
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
+  # PATCH/PUT /users/1
   def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
+  # DELETE /users/1
   def destroy
+    @user.destroy
   end
 end
+
+private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(
+      :location_id,
+      :first_name,
+      :last_name,
+      :username,
+      :email,
+      :password,
+      :address,
+      :zipcode
+      )
+  end
