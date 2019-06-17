9c5f60d4decb04fcedafb5c37bb513b34cfac629
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 23:43:34 -0400

    Added private methods set_ingredient & ingredient_params. Removed unnecesary actions (new and edit). Added relevant code to actions to render the data as JSON.

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -1,22 +1,54 @@
 class Api::V1::IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:show, :update, :destroy]
+
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @ingredients = Ingredient.all
+    render json: @ingredients
   end
 
   def show
+    render json: @ingredient
   end
 
-  def edit
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+
+    if @ingredient.save
+      render json: @ingredient, status: :created, location: @ingredient
+    else
+      render json: @ingredient.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @ingredient.save
+      render json: @ingredient, status: :accepted
+    else
+      render json: @ingredient.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @ingredient.destroy
   end
 end
+
+private
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(
+      :category_id,
+      :name,
+      :calories,
+      :fat,
+      :cholesterol,
+      :sodium,
+      :carbohydrates,
+      :sugar,
+      :protein
+    )
+  end
