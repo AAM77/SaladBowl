abce949e7b1dac5055cbd65a10d42f963cf9e770
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 23:43:02 -0400

    Added private methods set_salad_bar_location & salad_bar_location_params. Removed unnecesary actions (new and edit). Added relevant code to actions to render the data as JSON.

--- app/controllers/api/v1/salad_bar_locations_controller.rb
+++ app/controllers/api/v1/salad_bar_locations_controller.rb
@@ -1,22 +1,46 @@
 class Api::V1::SaladBarLocationsController < ApplicationController
+  before_action :set_salad_bar_location, only: [:show, :update, :destroy]
+
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @salad_bar_locations = SaladBarLocation.all
+    render json: @salad_bar_locations
   end
 
   def show
+    render json: @salad_bar_location
   end
 
-  def edit
+  def create
+    @salad_bar_location = SaladBarLocation.new(salad_bar_location_params)
+
+    if @salad_bar_location.save
+      render json: @salad_bar_location, status: :created, location: @salad_bar_location
+    else
+      render json: @salad_bar_location.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @salad_bar_location.save
+      render json: @salad_bar_location, status: :accepted
+    else
+      render json: @salad_bar_location.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @salad_bar_location.destroy
   end
 end
+
+private
+
+  def set_salad_bar_location
+    @salad_bar_location = SaladBarLocation.find(params[:id])
+  end
+
+  def salad_bar_location_params
+    params.require(:salad_bar_location).permit(
+      :name
+    )
+  end
