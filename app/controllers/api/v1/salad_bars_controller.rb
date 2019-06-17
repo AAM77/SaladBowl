e854d705681cfd3dca44e1af5dc02506b74d1ecb
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 23:42:40 -0400

    Added private methods set_salad_bar & salad_bar_params. Removed unnecesary actions (new and edit). Added relevant code to actions to render the data as JSON.

--- app/controllers/api/v1/salad_bars_controller.rb
+++ app/controllers/api/v1/salad_bars_controller.rb
@@ -1,22 +1,46 @@
 class Api::V1::SaladBarsController < ApplicationController
+  before_action :set_salad_bar, only: [:show, :update, :destroy]
+
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @salad_bars = SaladBar.all
+    render json: @salad_bars
   end
 
   def show
+    render json: @salad_bar
   end
 
-  def edit
+  def create
+    @salad_bar = SaladBar.new(salad_bar_params)
+
+    if @salad_bar.save
+      render json: @salad_bar, status: :created, location: @salad_bar
+    else
+      render json: @salad_bar.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @salad_bar.save
+      render json: @salad_bar, status: :accepted
+    else
+      render json: @salad_bar.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @salad_bar.destroy
   end
 end
+
+private
+
+  def set_salad_bar
+    @salad_bar = SaladBar.find(params[:id])
+  end
+
+  def salad_bar_params
+    params.require(:salad_bar).permit(
+      :name
+    )
+  end
