6955ccc9837b8c516a392e41025d967ea4519ea4
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-16 23:43:58 -0400

    Added private methods set_bowl_ingredient & bowl_ingredient_params. Removed unnecesary actions (new and edit). Added relevant code to actions to render the data as JSON.

--- app/controllers/api/v1/bowl_ingredients_controller.rb
+++ app/controllers/api/v1/bowl_ingredients_controller.rb
@@ -1,22 +1,47 @@
 class Api::V1::BowlIngredientsController < ApplicationController
+  before_action :set_bowl_ingredient, only: [:show, :update, :destroy]
+
   def index
-  end
-
-  def new
-  end
-
-  def create
+    @bowl_ingredients = BowlIngredient.all
+    render json: @bowl_ingredients
   end
 
   def show
+    render json: @bowl_ingredient
   end
 
-  def edit
+  def create
+    @bowl_ingredient = BowlIngredient.new(bowl_ingredient_params)
+
+    if @bowl_ingredient.save
+      render json: @bowl_ingredient, status: :created, location: @bowl_ingredient
+    else
+      render json: @bowl_ingredient.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @bowl_ingredient.save
+      render json: @bowl_ingredient, status: :accepted
+    else
+      render json: @bowl_ingredient.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @bowl_ingredient.destroy
   end
 end
+
+private
+
+  def set_bowl_ingredient
+    @bowl_ingredient = BowlIngredient.find(params[:id])
+  end
+
+  def bowl_ingredient_params
+    params.require(:bowl_ingredient).permit(
+      :salad_bowl_id,
+      :ingredient_id
+    )
+  end
