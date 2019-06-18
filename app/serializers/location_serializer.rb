ac34979c26a41138d5016c4854d35f2feff3e74b
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-17 23:45:34 -0400

    Refactored the users_at_location method. Added a salad_bars_at_location method to display the salad_bars at the location.

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,8 +1,11 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :city, :state, :country, :users_at_location
+  attributes :id, :city, :state, :country, :users_at_location, :salad_bars_at_location
 
   def users_at_location
-    loc = Location.find(object.id)
-    loc.users.map { |u| u.id }.empty? ? 'None' : loc.users.map { |u| u.id }
+    object.users.map { |u| u.id }.empty? ? 'None' : object.users.map { |u| u.id }
+  end
+
+  def salad_bars_at_location
+    object.salad_bars.empty? ? 'None' : object.salad_bars
   end
 end
