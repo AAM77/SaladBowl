3bb521c8ecaf9ac1f91d34cee397ff24379bd42a
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-21 20:20:12 -0400

    Created a custom category serializer to display the name and id of all ingredients in that category.

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,7 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :category_ingredients
+
+  def category_ingredients
+    object.ingredients.empty? ? 'None' : object.ingredients.map { |i| { id: i.id, name: i.name } }
+  end
 end
