5f66892e71e42855fbc47c536c29eda9fc428f93
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-22 23:41:11 -0400

    Added attributes, a method to diplay users (id and username only)

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,11 +1,11 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :city, :state, :country, :users_at_location, :salad_bars_at_location
+  attributes :id, :city, :state, :country, :users_at_location, :salad_bars
 
   def users_at_location
-    object.users.map { |u| u.id }.empty? ? 'None' : object.users.map { |u| u.id }
+    object.users.map { |u| u.id }.empty? ? 'None' : object.users.map { |u| { id: u.id, username: u.username } }
   end
 
-  def salad_bars_at_location
-    object.salad_bars.empty? ? 'None' : object.salad_bars
-  end
+  # def salad_bars_at_location
+  #   object.salad_bars.empty? ? 'None' : object.salad_bars
+  # end
 end
