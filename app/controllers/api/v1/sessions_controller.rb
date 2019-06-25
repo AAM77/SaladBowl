89e1d37bdeb50a9db50518484f458694ae8b5581
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-06-25 00:22:37 -0400

    Generated a sessions controller. Added the create & destroy actions. Added code to handle authentication and errors.

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
+  end
+
+  def destroy
+  end
+end
