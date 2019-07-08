a83252c3b953c02b55512e0ae0326a184ff3e070
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-08 13:17:31 -0400

    Formated data before sending to json.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,20 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :username, :email, :address, :zipcode
+
+  def first_name
+    object.first_name.capitalize
+  end
+
+  def last_name
+    object.last_name.capitalize
+  end
+
+  def username
+    object.username.capitalize
+  end
+
+  def email
+    object.email.capitalize
+  end
+  
 end
