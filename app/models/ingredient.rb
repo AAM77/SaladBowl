850321efc40734bcebd6e10b275e59c9752e8dc5
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-08 14:20:48 -0400

    Added a uniqueness validator to the ingredient model.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,6 @@ class Ingredient < ApplicationRecord
   belongs_to :category
   has_many :bowl_ingredients, dependent: :destroy
   has_many :salad_bowls, through: :bowl_ingredients
+
+  validates :name, uniqueness: true
 end
