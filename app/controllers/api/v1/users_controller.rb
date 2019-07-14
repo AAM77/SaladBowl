f248734bf89802611982d39c1bf28c288f6c6e61
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-13 22:54:22 -0400

    added to the destroy action. removed downcase from form_params username & email. Commented out user_params parser for the update action. Removed unnecessary commented out code.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,37 +13,33 @@ class Api::V1::UsersController < ApplicationController
 
   # POST /users
   def create
-    # existing_username = User.find_by(username: form_data[:username])
-    # existing_email = User.find_by(email: form_data[:email])
 
     @user = User.new(form_data)
     @user.location_id = 1; # TEMPORARY --- CHANGE LATER!!!
 
-    # if existing_username || existing_email
-    #   render json: @user.errors, status: "Invalid Username or Password"
-    # else
-      if @user.save
-        session[:user_id] = @user.id
-        render json: UserSerializer.new(@user), status: :created
-      else
-        resp = {
-          error: @user.errors.full_messages.to_sentence
-        }
-        render json: resp, status: :unprocessable_entity
-      end
-    # end
+
+    if @user.save
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
+    else
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
+    end
+
   end
 
   # PATCH/PUT /users/1
   def update
-    new_params = {}
-    user_params.each do |user_param|
-      if !user_param[1].empty? && !user_param[1].nil?
-        new_params[user_param[0]] = user_param[1]
-      end
-    end
+    # new_params = {}
+    # user_params.each do |user_param|
+    #   if !user_param[1].empty? && !user_param[1].nil?
+    #     new_params[user_param[0]] = user_param[1]
+    #   end
+    # end
 
-    if @user.update(new_params)
+    if @user.update(user_params)
       render json: @user, status: :accepted
     else
       render json: @user.errors, status: :unprocessable_entity
@@ -54,6 +50,9 @@ class Api::V1::UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     @user.destroy
+    render json: {
+      notice: "Successfully deleted"
+    }, status: :ok
   end
 end
 
@@ -71,8 +70,8 @@ private
       location_id: 1,
       first_name: params[:first_name],
       last_name: params[:last_name],
-      username: params[:username].downcase,
-      email: params[:email].downcase,
+      username: params[:username],
+      email: params[:email],
       password: params[:password],
       address: params[:address],
       zipcode: params[:zipcode]
