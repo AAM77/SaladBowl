512bcde4af3395ac650278e75a12c2f752e3f3e9
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-07-13 22:54:56 -0400

    Added conditionals to avoid errors when attempting to capitalize nil.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,19 +2,19 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :username, :email, :address, :zipcode
 
   def first_name
-    object.first_name.capitalize
+    object.first_name.capitalize if object.first_name
   end
 
   def last_name
-    object.last_name.capitalize
+    object.last_name.capitalize if object.last_name
   end
 
   def username
-    object.username.capitalize
+    object.username.capitalize if object.username
   end
 
   def email
-    object.email.capitalize
+    object.email.capitalize if object.email
   end
-  
+
 end
